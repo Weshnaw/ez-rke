a3b234fcc77f405833e3a826c3c009f393a7a062
Weshnaw <bfallquist@gmail.com>
2024-09-07 05:33:45 -0400

    better logging

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,167 @@
 use std::{
+    collections::HashMap,
+    fmt::Display,
     fs::OpenOptions,
     io,
     sync::{Arc, Mutex, PoisonError},
     time::Duration,
 };
 
+use chrono::{DateTime, Local};
+use colored::Colorize;
 use ratatui::{
     crossterm::event::{self, Event, KeyCode},
     widgets::{Block, Paragraph},
     Frame,
 };
 use tokio::time::sleep;
-use tracing::{debug, info, instrument, trace};
+use tracing::{
+    debug,
+    field::{Field, Visit},
+    info, instrument, trace, Level,
+};
 use tracing_subscriber::{
-    fmt::{self, MakeWriter},
+    fmt,
     layer::SubscriberExt,
+    registry::{LookupSpan, SpanRef},
     util::SubscriberInitExt,
-    EnvFilter,
+    EnvFilter, Layer,
 };
 
-struct TuiLogger {
-    tx: flume::Sender<Box<[u8]>>,
+struct TuiLayer<T> {
+    tx: flume::Sender<T>,
 }
 
-impl TuiLogger {
-    fn new() -> (Self, flume::Receiver<Box<[u8]>>) {
+impl<T> TuiLayer<T>
+where
+    T: Send + Sync + 'static,
+{
+    fn new() -> (Self, Arc<Mutex<Vec<T>>>) {
         let (tx, rx) = flume::unbounded();
 
-        (Self { tx }, rx)
+        let logs = Arc::new(Mutex::new(vec![]));
+
+        let result = logs.clone();
+        tokio::spawn(async move {
+            while let Ok(event) = rx.recv_async().await {
+                let mut logs = logs.lock().unwrap_or_else(PoisonError::into_inner);
+                logs.push(event);
+            }
+        });
+
+        (Self { tx }, result)
     }
 }
 
-// I've seen other folks use a sink, is that needed?
-impl<'a> io::Write for &'a TuiLogger {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.tx
-            .send(buf.into())
-            .map_err(|e| io::Error::new(io::ErrorKind::Other, e))?;
-
-        Ok(buf.len())
-    }
-
-    fn flush(&mut self) -> io::Result<()> {
-        Ok(())
-    }
+#[derive(Debug, Default)]
+struct LogSpan {
+    //target: Box<str>,
+    //name: Box<str>,
+    //fields: Fields,
+    scope: Arc<str>,
 }
 
-impl<'a> MakeWriter<'a> for TuiLogger {
-    type Writer = &'a TuiLogger;
+#[derive(Debug)]
+struct LogEvent {
+    level: Level,
+    target: Box<str>,
+    name: Box<str>,
+    fields: Fields,
+    timestamp: DateTime<Local>,
+    span: Option<LogSpan>,
+}
 
-    fn make_writer(&'a self) -> Self::Writer {
+impl LogEvent {
+    fn with_span(mut self, span: LogSpan) -> Self {
+        self.span = Some(span);
+
         self
+    }
+}
+
+#[derive(Debug)]
+struct Fields(HashMap<Box<str>, Box<str>>);
+
+impl Visit for Fields {
+    fn record_debug(&mut self, field: &Field, value: &dyn std::fmt::Debug) {
+        self.0
+            .insert(field.name().into(), format!("{:?}", value).into());
+    }
+}
+
+impl<'a, R> From<SpanRef<'a, R>> for LogSpan
+where
+    R: LookupSpan<'a>,
+{
+    fn from(span: SpanRef<'a, R>) -> Self {
+        let scope = span
+            .scope()
+            .map(|span| span.name())
+            .collect::<Vec<_>>()
+            .join(":")
+            .into();
+        Self { scope }
+    }
+}
+
+impl<'a> From<&'a tracing::Event<'a>> for LogEvent {
+    fn from(value: &'a tracing::Event<'a>) -> Self {
+        let meta = value.metadata();
+        let level = meta.level().to_owned();
+        let target = meta.target().into();
+        let name = meta.name().into();
+        let mut fields = Fields(HashMap::new());
+        value.record(&mut fields);
+
+        let timestamp = Local::now();
+
+        Self {
+            level,
+            target,
+            name,
+            fields,
+            timestamp,
+            span: None,
+        }
+    }
+}
+
+impl Display for LogEvent {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let scope = if let Some(span) = &self.span {
+            format!("{}:{}", self.target, span.scope)
+        } else {
+            self.target.to_string()
+        };
+
+        let timestamp = self.timestamp.format("[%Y-%m-%d][%H:%M:%S]");
+
+        let _name = self.name.as_ref();
+
+        let level = match self.level {
+            Level::INFO => "INFO".green(),
+            Level::DEBUG => "DEBUG".white(),
+            Level::TRACE => "TRACE".yellow(),
+            Level::WARN => "WARN".red(),
+            Level::ERROR => "ERROR".red()
+        };
+
+        let fields = &self.fields.0;
+
+        write!(f, "{timestamp} {level:5} {scope:30.30} {fields:?}",)
+    }
+}
+
+impl<S> Layer<S> for TuiLayer<LogEvent>
+where
+    S: tracing::Subscriber + for<'a> LookupSpan<'a>,
+{
+    fn on_event(&self, event: &tracing::Event<'_>, ctx: tracing_subscriber::layer::Context<'_, S>) {
+        let mut event: LogEvent = event.into();
+        if let Some(span) = ctx.lookup_current() {
+            event = event.with_span(span.into())
+        }
+        self.tx.send(event).ok();
     }
 }
 
@@ -62,15 +173,14 @@ async fn main() -> io::Result<()> {
         .open("ez_rke.log")
         .unwrap();
 
-    let (tui_logger, rx) = TuiLogger::new();
+    let (tui_layer, logs) = TuiLayer::new();
 
     tracing_subscriber::registry()
         .with(fmt::layer().json().with_writer(logging_file))
-        .with(fmt::layer().with_writer(tui_logger))
+        .with(tui_layer)
         .with(EnvFilter::from_default_env())
         .init();
 
-    let logs = init_tui_logs(rx);
     info!("Initialized ez_rke loggers...");
 
     let mut terminal = ratatui::init();
@@ -103,27 +213,11 @@ fn handle_events(exit: &mut bool) -> io::Result<()> {
             *exit = KeyCode::Char('q') == key.code;
         }
     }
-    //events.truncate(10);
     Ok(())
 }
 
 #[instrument(skip_all)]
-fn init_tui_logs(rx: flume::Receiver<Box<[u8]>>) -> Arc<Mutex<Vec<Box<str>>>> {
-    let logs = Arc::new(Mutex::new(vec![]));
-    
-    let result = logs.clone();
-    tokio::spawn(async move {
-        while let Ok(event) = rx.recv_async().await {
-            let mut logs = logs.lock().unwrap_or_else(PoisonError::into_inner);
-            logs.push(std::str::from_utf8(&event).unwrap_or_default().into());
-        }
-    });
-
-    result
-}
-
-#[instrument(skip_all)]
-fn draw(frame: &mut Frame, logs: Arc<Mutex<Vec<Box<str>>>>) {
+fn draw(frame: &mut Frame, logs: Arc<Mutex<Vec<LogEvent>>>) {
     // To view this event, run the example with `RUST_LOG=tracing=debug cargo run --example tracing`
     trace!(frame_count = frame.count());
     let height = frame.area().height;
@@ -133,10 +227,10 @@ fn draw(frame: &mut Frame, logs: Arc<Mutex<Vec<Box<str>>>>) {
         .iter()
         .rev()
         .take(height.into())
-        .map(|s| s.as_ref())
+        .map(|s| format!("{s}"))
         .rev()
-        .collect::<Box<[&str]>>();
-    let paragraph = Paragraph::new(logs.join(""))
+        .collect::<Box<[String]>>();
+    let paragraph = Paragraph::new(logs.join("\n"))
         .block(Block::bordered().title("Tracing example. Press 'q' to quit."));
     frame.render_widget(paragraph, frame.area());
 }
